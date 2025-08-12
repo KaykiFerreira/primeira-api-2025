3a82cb0accbc634a0ae720b20ec5307fb0edd750
KaykiFerreira <kaykiclasrhoyale30@gmail.com>
2025-08-12 16:19:02 -0300

    sla3

--- src/main/java/application/controller/IndexController.java
+++ src/main/java/application/controller/IndexController.java
@@ -1,19 +1,28 @@
 package application.controller;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/")
 public class IndexController {
-    private List<String> frutas = Arrays.asList("Maça", "Uva", "Banana");
+    //private List<String> frutas = Arrays.asList("Maça", "Uva", "Banana");
+    private List<String> frutas = new ArrayList<>();
 
     @GetMapping
     public List<String> home() {
+        return frutas;
+    }
+
+    @PostMapping
+    public List<String> insert(@RequestBody String fruta) {
+        frutas.add(fruta);
         return frutas;
     }
 }
